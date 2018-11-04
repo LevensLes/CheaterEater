4c0e03d1ce4e935689e1f782de4051dedf69cb1d
LevensLes <32346039+LevensLes@users.noreply.github.com>
2018-11-04 13:39:11 +0100

    Delete drivel.vb

--- Cheater Eater/Cheater Eater/drivel.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class drivel
-    Private Sub FlatButton2_Click(sender As Object, e As EventArgs) Handles FlatButton2.Click
-        Dim drive As String = FlatListBox1.SelectedItem
-        Process.Start("explorer.exe", drive)
-    End Sub
-End Class
