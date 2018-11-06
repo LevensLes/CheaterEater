952b8d4613b1ad9198fa8ced9edc072834c8e7f6
LevensLes <32346039+LevensLes@users.noreply.github.com>
2018-11-06 14:36:57 +0100

    Added missing files

---
+++ Cheater Eater/Cheater Eater/drivel.vb
@@ -0,0 +1,6 @@
+﻿Public Class drivel
+    Private Sub FlatButton2_Click(sender As Object, e As EventArgs) Handles FlatButton2.Click
+        Dim drive As String = FlatListBox1.SelectedItem
+        Process.Start("explorer.exe", drive)
+    End Sub
+End Class
